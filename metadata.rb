e934ae89f7d39b8561363e3038a5ce30356ec70d
Ryan Gribble <ryangribble@users.noreply.github.com>
2016-10-20 09:49:04 +1000

    Update metadata to set a custom name
    To avoid conflicts with community cookbook on supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name 'octopus-deploy'
+name 'octopus-deploy-tattsgroup'
 maintainer 'Montague, Brent'
 maintainer_email 'BMontague@cvent.com'
 license 'Apache 2.0'
-description 'Handles installing Octopus Deploy Server &| Tentacle'
+description '(TattsGroup Fork) Handles installing Octopus Deploy Server &| Tentacle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/cvent/octopus-deploy-cookbook'
-issues_url 'https://github.com/cvent/octopus-deploy-cookbook/issues'
-version '0.6.3'
+source_url 'https://github.com/TattsGroup/octopus-deploy-cookbook'
+issues_url 'https://github.com/TattsGroup/octopus-deploy-cookbook/issues'
+version '0.6.4'
 
 depends 'windows', '~> 1.38'
 depends 'windows_firewall', '~> 3.0.2'
